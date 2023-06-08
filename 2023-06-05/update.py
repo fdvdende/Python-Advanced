03fde7a87497be1bae10f2ca5ff491d85ee736f6
Peter <email@peteranema.nl>
2023-06-08 10:24:02 +0200

    Update update.py

--- 2023-06-05/update.py
+++ 2023-06-05/update.py
@@ -19,3 +19,14 @@ from src.persistence.store_in_sqlite import purge
 
 if __name__ == '__main__':
     purge()
+
+# in flask_ui.py
+
+# line 5
+from ..persistence.store_in_sqlite import load, update_status, purge
+
+# line 85
+def main():
+    purge()
+    app.run(host='0.0.0.0', port=8080)
+
