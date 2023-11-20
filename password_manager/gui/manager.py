89e663ff54c20f55cdf6f8ffa2cd9f1620df1242
Peter <email@peteranema.nl>
2023-11-20 11:10:38 +0100

    Update

---
+++ password_manager/gui/manager.py
@@ -0,0 +1,17 @@
+import tkinter as tk
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+
+    root = tk.Tk()
+
+    root.geometry('300x400+100+100')  # width x height + x_offset + y_offset
+    root.title('Password Manager')
+
+    root.mainloop()
