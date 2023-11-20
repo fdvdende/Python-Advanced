7ea4408e1c4e2dcb541b574b08366a2a53541f6f
Peter <email@peteranema.nl>
2023-11-20 11:28:19 +0100

    Update manager.py

--- password_manager/gui/manager.py
+++ password_manager/gui/manager.py
@@ -11,14 +11,26 @@ class App(tk.Frame):
         lbl_name = tk.Label(text='Naam', justify=tk.LEFT)
         lbl_name.grid(row = 1, column = 1, ipadx = 16)
 
+        entry_name = tk.Entry()
+        entry_name.grid(row = 1, column = 2, ipadx = 16)
+
         lbl_url = tk.Label(text='URL')
         lbl_url.grid(row = 2, column = 1, ipadx = 16)
+
+        entry_url = tk.Entry()
+        entry_url.grid(row = 2, column = 2, ipadx = 16)
 
         lbl_username = tk.Label(text='Username')
         lbl_username.grid(row = 3, column = 1, ipadx = 16)
 
+        entry_username = tk.Entry()
+        entry_username.grid(row = 3, column = 2, ipadx = 16)
+
         lbl_password = tk.Label(text='Password')
         lbl_password.grid(row = 4, column = 1, ipadx = 16)
+
+        entry_password = tk.Entry()
+        entry_password.grid(row = 4, column = 2, ipadx = 16)
 
 
 
