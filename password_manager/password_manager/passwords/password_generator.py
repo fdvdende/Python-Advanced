3a07876d358cf4f746565aa9732319470f52fc13
Peter <email@peteranema.nl>
2023-11-20 10:33:29 +0100

    Update

--- password_manager/password_manager/passwords/password_generator.py
+++ password_manager/password_manager/passwords/password_generator.py
@@ -1,16 +1,25 @@
 import random
 
-def generate_password(
-    required_length = 8,
-    n_lowercase = 2,
-    n_uppercase = 2,
-    n_numbers = 1,
-    n_special = 1):
 
-    lowercase_characters = 'abcdefghijklmnopqrstuvwxyz'
-    uppercase_characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+class InvalidArgumentException(Exception):
+    pass
+
+
+def generate_password(required_length=8,
+                      n_lowercase=1,
+                      n_uppercase=1,
+                      n_numbers=1,
+                      n_special=1):
+
+    lowercase_characters = 'abcdefghijkmnopqrstuvwxyz'  # removed l
+    uppercase_characters = 'ABCDEFGHJKLMNPQRSTUVWXYZ'  # removed IO
     number_characters = '0123456789'
     special_characters = '@#$%&*+?!'
+
+    n_extra = required_length - n_lowercase - n_uppercase - n_numbers - n_special
+
+    if n_extra < 0:
+        raise InvalidArgumentException('Required length is to low.')
 
     lower = random.choices(lowercase_characters, k=n_lowercase)
     upper = random.choices(uppercase_characters, k=n_uppercase)
@@ -20,11 +29,19 @@ def generate_password(
                            uppercase_characters +
                            number_characters +
                            special_characters,
-                           k=required_length - n_lowercase - n_uppercase - n_numbers - n_special)
+                           k=n_extra)
 
     all = lower + upper + numbers + special + extra
+
     random.shuffle(all)
 
     password = ''.join(all)
 
     return password
+
+
+# -----------------------
+
+if __name__ == '__main__':
+
+    print(generate_password(8, 4, 4, 0, 0))
