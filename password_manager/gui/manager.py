666758a34898490060990d580aa5bb99b8cae7a5
Peter <email@peteranema.nl>
2023-11-20 11:26:01 +0100

    Update manager.py

--- password_manager/gui/manager.py
+++ password_manager/gui/manager.py
@@ -1,7 +1,24 @@
 import tkinter as tk
 
 
+class App(tk.Frame):
 
+    def __init__(self, master):
+        self.create_widgets(master)
+
+    def create_widgets(self, master):
+
+        lbl_name = tk.Label(text='Naam', justify=tk.LEFT)
+        lbl_name.grid(row = 1, column = 1, ipadx = 16)
+
+        lbl_url = tk.Label(text='URL')
+        lbl_url.grid(row = 2, column = 1, ipadx = 16)
+
+        lbl_username = tk.Label(text='Username')
+        lbl_username.grid(row = 3, column = 1, ipadx = 16)
+
+        lbl_password = tk.Label(text='Password')
+        lbl_password.grid(row = 4, column = 1, ipadx = 16)
 
 
 
@@ -13,5 +30,7 @@ if __name__ == '__main__':
 
     root.geometry('300x400+100+100')  # width x height + x_offset + y_offset
     root.title('Password Manager')
+
+    App(root)
 
     root.mainloop()
