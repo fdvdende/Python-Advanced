7d8a4b77accb8266c653a17297ab697435a2beb9
Peter <email@peteranema.nl>
2023-11-20 12:53:01 +0100

    Update manager.py

--- password_manager/gui/manager.py
+++ password_manager/gui/manager.py
@@ -35,48 +35,53 @@ class App(tk.Frame):
         entry_password = tk.Entry()
         entry_password.grid(row = 4, column = 2, ipadx = 16)
 
+        btn_generate = tk.Button(text='Store', font=('Helvetica', 18, 'bold'), fg='#aa0000')
+        btn_generate.grid(row = 5, column = 1, columnspan=2, pady = 16, ipadx = 8, ipady = 8)
+
         lbl_name = tk.Label(text='Password generator', font=('Helvetica', 24, 'bold'))
-        lbl_name.grid(row = 5, column = 1, columnspan = 2, ipadx = 16, pady = (32, 6))
+        lbl_name.grid(row = 6, column = 1, columnspan = 2, ipadx = 16, pady = (12, 6))
 
         lbl_length = tk.Label(text='Number of characters')
-        lbl_length.grid(row = 6, column = 1, ipadx = 16, sticky='w')
+        lbl_length.grid(row = 7, column = 1, ipadx = 16, sticky='w')
 
-        entry_length = tk.Entry(width = 6)
-        entry_length.grid(row = 6, column = 2, ipadx = 16)
+        # entry_length = tk.Entry(width = 6)
+        entry_length = tk.Scale(master, from_=4, to=20, orient=tk.HORIZONTAL)
+        entry_length.set(8)
+        entry_length.grid(row = 7, column = 2, ipadx = 16, sticky='w')
 
         lbl_uppercase = tk.Label(text='Number of uppercase')
-        lbl_uppercase.grid(row = 7, column = 1, ipadx = 16, sticky='w')
+        lbl_uppercase.grid(row = 8, column = 1, ipadx = 16, sticky='w')
 
         entry_uppercase = tk.Entry(width = 6)
-        entry_uppercase.grid(row = 7, column = 2, ipadx = 16)
+        entry_uppercase.grid(row = 8, column = 2, ipadx = 16, sticky='w')
 
         lbl_lowercase = tk.Label(text='Number of lowercase')
-        lbl_lowercase.grid(row = 8, column = 1, ipadx = 16, sticky='w')
+        lbl_lowercase.grid(row = 9, column = 1, ipadx = 16, sticky='w')
 
         entry_lowercase = tk.Entry(width = 6)
-        entry_lowercase.grid(row = 8, column = 2, ipadx = 16)
+        entry_lowercase.grid(row = 9, column = 2, ipadx = 16, sticky='w')
 
         lbl_digits = tk.Label(text='Number of digits')
-        lbl_digits.grid(row = 9, column = 1, ipadx = 16, sticky='w')
+        lbl_digits.grid(row = 10, column = 1, ipadx = 16, sticky='w')
 
         entry_digits = tk.Entry(width = 6)
-        entry_digits.grid(row = 9, column = 2, ipadx = 16)
+        entry_digits.grid(row = 10, column = 2, ipadx = 16, sticky='w')
 
         lbl_special = tk.Label(text='Number of specials')
-        lbl_special.grid(row = 10, column = 1, ipadx = 16, sticky='w')
+        lbl_special.grid(row = 11, column = 1, ipadx = 16, sticky='w')
 
         entry_special = tk.Entry(width = 6)
-        entry_special.grid(row = 10, column = 2, ipadx = 16)
+        entry_special.grid(row = 11, column = 2, ipadx = 16, sticky='w')
 
-        btn_generate = tk.Button(text='Generate', font=('Helvetica', 16, 'bold'))
-        btn_generate.grid(row = 11, column = 1, columnspan=2, pady = 16, ipadx = 8, ipady = 8)
+        btn_generate = tk.Button(text='Generate', font=('Helvetica', 18, 'bold'), fg='#aa0000')
+        btn_generate.grid(row = 12, column = 1, columnspan=2, pady = 16, ipadx = 8, ipady = 8)
 
 
 if __name__ == '__main__':
 
     root = tk.Tk()
 
-    root.geometry('410x480+100+100')  # width x height + x_offset + y_offset
+    root.geometry('410x550+100+100')  # width x height + x_offset + y_offset
     root.title('Password Manager')
 
     App(root)
