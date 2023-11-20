59fbba2994e13e452ced19bd04e842a5d5e4d06f
Peter <email@peteranema.nl>
2023-11-20 11:39:51 +0100

    Update manager.py

--- password_manager/gui/manager.py
+++ password_manager/gui/manager.py
@@ -8,7 +8,7 @@ class App(tk.Frame):
 
     def create_widgets(self, master):
 
-        lbl_name = tk.Label(text='Naam', justify=tk.LEFT)
+        lbl_name = tk.Label(text='Naam', justify=tk.LEFT, font=('Arial', 14))
         lbl_name.grid(row = 1, column = 1, ipadx = 16)
 
         entry_name = tk.Entry()
@@ -32,7 +32,38 @@ class App(tk.Frame):
         entry_password = tk.Entry()
         entry_password.grid(row = 4, column = 2, ipadx = 16)
 
+        lbl_name = tk.Label(text='Password generator', justify=tk.LEFT)
+        lbl_name.grid(row = 5, column = 1, ipadx = 16, pady = (16, 0))
 
+        lbl_length = tk.Label(text='Number of characters')
+        lbl_length.grid(row = 6, column = 1, ipadx = 16)
+
+        entry_length = tk.Entry(width = 6)
+        entry_length.grid(row = 6, column = 2, ipadx = 16)
+
+        lbl_uppercase = tk.Label(text='Number of uppercase')
+        lbl_uppercase.grid(row = 7, column = 1, ipadx = 16)
+
+        entry_uppercase = tk.Entry(width = 6)
+        entry_uppercase.grid(row = 7, column = 2, ipadx = 16)
+
+        lbl_lowercase = tk.Label(text='Number of lowercase')
+        lbl_lowercase.grid(row = 8, column = 1, ipadx = 16)
+
+        entry_lowercase = tk.Entry(width = 6)
+        entry_lowercase.grid(row = 8, column = 2, ipadx = 16)
+
+        lbl_digits = tk.Label(text='Number of digits')
+        lbl_digits.grid(row = 9, column = 1, ipadx = 16)
+
+        entry_digits = tk.Entry(width = 6)
+        entry_digits.grid(row = 9, column = 2, ipadx = 16)
+
+        lbl_special = tk.Label(text='Number of special')
+        lbl_special.grid(row = 10, column = 1, ipadx = 16)
+
+        entry_special = tk.Entry(width = 6)
+        entry_special.grid(row = 10, column = 2, ipadx = 16)
 
 
 
@@ -40,7 +71,7 @@ if __name__ == '__main__':
 
     root = tk.Tk()
 
-    root.geometry('300x400+100+100')  # width x height + x_offset + y_offset
+    root.geometry('400x400+100+100')  # width x height + x_offset + y_offset
     root.title('Password Manager')
 
     App(root)
