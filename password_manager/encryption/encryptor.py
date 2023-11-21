2e55eb521e6be906bf4231466078114b896960a6
Peter <email@peteranema.nl>
2023-11-21 10:34:35 +0100

    Update

--- password_manager/encryption/encryptor.py
+++ password_manager/encryption/encryptor.py
@@ -1,19 +1,30 @@
 
 from cryptography.fernet import Fernet
 
-key = Fernet.generate_key()
-print(key)
+key = b'O7t1sce1zWza1YY-RtJMPygbnjFgBc47g2SVtGmynzw='
 
-f = Fernet(key)
+def encrypt(original):
+    pass
 
-original = b"A really secret message. Not for prying eyes."
-print(original)
+def decrypt(encrypted):
+    pass
 
-encrypted = f.encrypt(original)
-print(encrypted)
 
-decrypted = f.decrypt(encrypted)
-print(decrypted)
+if __name__ == '__main__':
+
+    key = Fernet.generate_key()
+    print(key)
+
+    f = Fernet(key)
+
+    original = b"A really secret message. Not for prying eyes."
+    print(original)
+
+    encrypted = f.encrypt(original)
+    print(encrypted)
+
+    decrypted = f.decrypt(encrypted)
+    print(decrypted)
 
 
 
